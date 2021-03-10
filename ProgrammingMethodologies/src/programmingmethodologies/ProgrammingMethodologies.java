cd89d44d9ce1dd1e99429c165b71eeb9cb20b1a0
kostaskouts <kostaskoutsintas@gmail.com>
2021-03-10 14:23:24 +0200

    69

--- ProgrammingMethodologies/src/programmingmethodologies/ProgrammingMethodologies.java
+++ ProgrammingMethodologies/src/programmingmethodologies/ProgrammingMethodologies.java
@@ -15,7 +15,7 @@ public class ProgrammingMethodologies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Online?");
+        System.out.println("toulhseisainoobas");
         // TODO code application logic here
     }
     
