fab2bf61262c8402d1fc589d413a92b0a6b0ddb5
themi <themi@192.168.0.101>
2021-03-10 14:19:34 +0200

    Checking

--- ProgrammingMethodologies/src/programmingmethodologies/ProgrammingMethodologies.java
+++ ProgrammingMethodologies/src/programmingmethodologies/ProgrammingMethodologies.java
@@ -15,6 +15,7 @@ public class ProgrammingMethodologies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Online?");
         // TODO code application logic here
     }
     
