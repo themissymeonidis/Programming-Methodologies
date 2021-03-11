16259d5e2dc6095c3d4813903c737f552f1b9b55
stasino <stasino9966@gmail.com>
2021-03-11 13:29:46 +0200

    nn

--- ProgrammingMethodologies/src/programmingmethodologies/ProgrammingMethodologies.java
+++ ProgrammingMethodologies/src/programmingmethodologies/ProgrammingMethodologies.java
@@ -15,7 +15,7 @@ public class ProgrammingMethodologies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("fidimegata99123");
+        System.out.println("fidimegata999");
         // TODO code application logic here
     }
     
